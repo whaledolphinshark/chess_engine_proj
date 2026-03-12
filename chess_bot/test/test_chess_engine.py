628ac595035bcb2e4312ab9ca9f93690be4d8301
whaledolphinshark <hongyilu123456@gmail.com>
2026-03-12 18:28:44 -0400

    added perft test mainly for testing promotions

--- chess_bot/test/test_chess_engine.py
+++ chess_bot/test/test_chess_engine.py
@@ -203,7 +203,7 @@ def test_board():
     set_board(board, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1")
     test_cases_2 = [(1, 48), (2, 2039), (3, 97862), (4, 4085603)]
     for plies, expected_positions in test_cases_2:
-        position_count = traverse_positions(board, plies, True)
+        position_count = traverse_positions(board, plies)
         assert position_count == expected_positions, f"{position_count} != {expected_positions}"
 
     set_board(board, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1")
@@ -215,12 +215,18 @@ def test_board():
     set_board(board, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1")
     test_cases_4 = [(1, 6), (2, 264), (3, 9467), (4, 422333)]
     for plies, expected_positions in test_cases_4:
-        position_count = traverse_positions(board, plies, True)
+        position_count = traverse_positions(board, plies)
         assert position_count == expected_positions, f"{position_count} != {expected_positions}"
 
     set_board(board, "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8")
     test_cases_5 = [(1, 44), (2, 1486), (3, 62379), (4, 2103487)]
     for plies, expected_positions in test_cases_5:
+        position_count = traverse_positions(board, plies)
+        assert position_count == expected_positions, f"{position_count} != {expected_positions}"
+
+    set_board(board, "n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1")
+    test_cases_6 = [(1, 24), (2, 496), (3, 9483), (4, 182838), (5, 3605103)]
+    for plies, expected_positions in test_cases_6:
         position_count = traverse_positions(board, plies, True)
         assert position_count == expected_positions, f"{position_count} != {expected_positions}"
 
