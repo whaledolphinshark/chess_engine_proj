dc3d0eb96a8a877c460a0bffb42a002ac059ad23
whaledolphinshark <hongyilu123456@gmail.com>
2026-03-17 20:33:44 -0400

    refactored code and it now times how long it takes

--- chess_bot/test/perft.c
+++ chess_bot/test/perft.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 #include "chess_engine/chess_types.h"
 #include "chess_engine/board.h"
@@ -40,170 +41,26 @@ void perft(_board *board, int ply, int collect_stats, int stats[5]){
     }
 }
 
-void pos_1(int collect_stats){
-    int stats[5] = {0, 0, 0, 0, 0};
-    _board *board = cb_create_board();
-    int expected_pos[5] = {20, 400, 8902, 197281, 4865609};
-    printf("position: %s\n", START_FEN);
-    for (int i = 1; i <= 5; i++){
-        perft(board, i, collect_stats, stats);
-        if (expected_pos[i - 1] != stats[0]){
-            printf("test not passed %d != %d\n", expected_pos[i - 1], stats[0]);
-        }
-        printf("depth: %d, nodes: %d", i, stats[0]);
-        if (collect_stats == 1){
-            printf(", captures: %d, en passants: %d, castles %d, promotions: %d", stats[1], stats[2], stats[3], stats[4]);
-        }
-        printf("\n\n");
-        stats[0] = stats[1] = stats[2] = stats[3] = stats[4] = 0;
-    }
-}
-
-void pos_2(int collect_stats){
-    char *fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
+void test_pos(int collect_stats, char *fen, int *expected_pos, int depth){
     int stats[5] = {0, 0, 0, 0, 0};
     _board *board = cb_create_board();
     cb_fen_to_board(board, fen);
-    int expected_pos[4] = {48, 2039, 97862, 4085603};
     printf("position: %s\n", fen);
-    for (int i = 1; i <= 4; i++){
+    for (int i = 1; i <= depth; i++){
+        clock_t start = clock();
         perft(board, i, collect_stats, stats);
+        clock_t end = clock();
         if (expected_pos[i - 1] != stats[0]){
             printf("test not passed %d != %d\n", expected_pos[i - 1], stats[0]);
         }
-        printf("depth: %d, nodes: %d", i, stats[0]);
+        printf("depth: %d, seconds: %f, nodes: %d", i, ((double) (end - start)) / CLOCKS_PER_SEC, stats[0]);
         if (collect_stats == 1){
             printf(", captures: %d, en passants: %d, castles %d, promotions: %d", stats[1], stats[2], stats[3], stats[4]);
         }
         printf("\n\n");
         stats[0] = stats[1] = stats[2] = stats[3] = stats[4] = 0;
     }
-}
-
-void pos_3(int collect_stats){
-    char *fen = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
-    int stats[5] = {0, 0, 0, 0, 0};
-    _board *board = cb_create_board();
-    cb_fen_to_board(board, fen);
-    int expected_pos[5] = {14, 191, 2812, 43238, 674624};
-    printf("position: %s\n", fen);
-    for (int i = 1; i <= 5; i++){
-        perft(board, i, collect_stats, stats);
-        if (expected_pos[i - 1] != stats[0]){
-            printf("test not passed %d != %d\n", expected_pos[i - 1], stats[0]);
-        }
-        printf("depth: %d, nodes: %d", i, stats[0]);
-        if (collect_stats == 1){
-            printf(", captures: %d, en passants: %d, castles %d, promotions: %d", stats[1], stats[2], stats[3], stats[4]);
-        }
-        printf("\n\n");
-        stats[0] = stats[1] = stats[2] = stats[3] = stats[4] = 0;
-    }
-}
-
-void pos_4(int collect_stats){
-    char *fen = "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1";
-    int stats[5] = {0, 0, 0, 0, 0};
-    _board *board = cb_create_board();
-    cb_fen_to_board(board, fen);
-    int expected_pos[4] = {6, 264, 9467, 422333};
-    printf("position: %s\n", fen);
-    for (int i = 1; i <= 4; i++){
-        perft(board, i, collect_stats, stats);
-        if (expected_pos[i - 1] != stats[0]){
-            printf("test not passed %d != %d\n", expected_pos[i - 1], stats[0]);
-        }
-        printf("depth: %d, nodes: %d", i, stats[0]);
-        if (collect_stats == 1){
-            printf(", captures: %d, en passants: %d, castles %d, promotions: %d", stats[1], stats[2], stats[3], stats[4]);
-        }
-        printf("\n\n");
-        stats[0] = stats[1] = stats[2] = stats[3] = stats[4] = 0;
-    }
-}
-
-void pos_5(int collect_stats){
-    char *fen = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
-    int stats[5] = {0, 0, 0, 0, 0};
-    _board *board = cb_create_board();
-    cb_fen_to_board(board, fen);
-    int expected_pos[3] = {44, 1486, 62379};
-    printf("position: %s\n", fen);
-    for (int i = 1; i <= 3; i++){
-        perft(board, i, collect_stats, stats);
-        if (expected_pos[i - 1] != stats[0]){
-            printf("test not passed %d != %d\n", expected_pos[i - 1], stats[0]);
-        }
-        printf("depth: %d, nodes: %d", i, stats[0]);
-        if (collect_stats == 1){
-            printf(", captures: %d, en passants: %d, castles %d, promotions: %d", stats[1], stats[2], stats[3], stats[4]);
-        }
-        printf("\n\n");
-        stats[0] = stats[1] = stats[2] = stats[3] = stats[4] = 0;
-    }
-}
-
-void pos_6(int collect_stats){
-    char *fen = "n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1";
-    int stats[5] = {0, 0, 0, 0, 0};
-    _board *board = cb_create_board();
-    cb_fen_to_board(board, fen);
-    int expected_pos[4] = {24, 496, 9483, 182838};
-    printf("position: %s\n", fen);
-    for (int i = 1; i <= 4; i++){
-        perft(board, i, collect_stats, stats);
-        if (expected_pos[i - 1] != stats[0]){
-            printf("test not passed %d != %d\n", expected_pos[i - 1], stats[0]);
-        }
-        printf("depth: %d, nodes: %d", i, stats[0]);
-        if (collect_stats == 1){
-            printf(", captures: %d, en passants: %d, castles %d, promotions: %d", stats[1], stats[2], stats[3], stats[4]);
-        }
-        printf("\n\n");
-        stats[0] = stats[1] = stats[2] = stats[3] = stats[4] = 0;
-    }
-}
-
-void pos_7(int collect_stats){
-    char *fen = "r3k2r/1bp2pP1/5n2/1P1Q4/1pPq4/5N2/1B1P2p1/R3K2R b KQkq c3 0 1";
-    int stats[5] = {0, 0, 0, 0, 0};
-    _board *board = cb_create_board();
-    cb_fen_to_board(board, fen);
-    int expected_pos[3] = {60, 2608, 113742};
-    printf("position: %s\n", fen);
-    for (int i = 1; i <= 3; i++){
-        perft(board, i, collect_stats, stats);
-        if (expected_pos[i - 1] != stats[0]){
-            printf("test not passed %d != %d\n", expected_pos[i - 1], stats[0]);
-        }
-        printf("depth: %d, nodes: %d", i, stats[0]);
-        if (collect_stats == 1){
-            printf(", captures: %d, en passants: %d, castles %d, promotions: %d", stats[1], stats[2], stats[3], stats[4]);
-        }
-        printf("\n\n");
-        stats[0] = stats[1] = stats[2] = stats[3] = stats[4] = 0;
-    }
-}
-
-void pos_8(int collect_stats){
-    char *fen = "8/K7/8/8/2Q1Pp1k/8/8/8 b - e3 0 1";
-    int stats[5] = {0, 0, 0, 0, 0};
-    _board *board = cb_create_board();
-    cb_fen_to_board(board, fen);
-    int expected_pos[5] = {6, 162, 937, 24947, 139087};
-    printf("position: %s\n", fen);
-    for (int i = 1; i <= 5; i++){
-        perft(board, i, collect_stats, stats);
-        if (expected_pos[i - 1] != stats[0]){
-            printf("test not passed %d != %d\n", expected_pos[i - 1], stats[0]);
-        }
-        printf("depth: %d, nodes: %d", i, stats[0]);
-        if (collect_stats == 1){
-            printf(", captures: %d, en passants: %d, castles %d, promotions: %d", stats[1], stats[2], stats[3], stats[4]);
-        }
-        printf("\n\n");
-        stats[0] = stats[1] = stats[2] = stats[3] = stats[4] = 0;
-    }
+    cb_destroy_board(board);
 }
 
 int main(int argc, char *argv[]){
@@ -224,14 +81,30 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "Usage %s [-s]\n-s: Enable statistics tracking\n", argv[0]);
         return 1;
     }
-    pos_1(collect_stats);
-    pos_2(collect_stats);
-    pos_3(collect_stats);
-    pos_4(collect_stats);
-    pos_5(collect_stats);
-    pos_6(collect_stats);
-    pos_7(collect_stats);
-    pos_8(collect_stats);
+
+    int expected_pos_1[5] = {20, 400, 8902, 197281, 4865609};
+    test_pos(collect_stats, START_FEN, expected_pos_1, 5);
+
+    int expected_pos_2[4] = {48, 2039, 97862, 4085603};
+    test_pos(collect_stats, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", expected_pos_2, 4);
+    
+    int expected_pos_3[5] = {14, 191, 2812, 43238, 674624};
+    test_pos(collect_stats, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", expected_pos_3, 5);
+
+    int expected_pos_4[4] = {6, 264, 9467, 422333};
+    test_pos(collect_stats, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", expected_pos_4, 4);
+
+    int expected_pos_5[3] = {44, 1486, 62379};
+    test_pos(collect_stats, "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", expected_pos_5, 3);
+
+    int expected_pos_6[4] = {24, 496, 9483, 182838};
+    test_pos(collect_stats, "n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1", expected_pos_6, 4);
+
+    int expected_pos_7[3] = {60, 2608, 113742};
+    test_pos(collect_stats, "r3k2r/1bp2pP1/5n2/1P1Q4/1pPq4/5N2/1B1P2p1/R3K2R b KQkq c3 0 1", expected_pos_7, 3);
+
+    int expected_pos_8[5] = {6, 162, 937, 24947, 139087};
+    test_pos(collect_stats, "8/K7/8/8/2Q1Pp1k/8/8/8 b - e3 0 1", expected_pos_8, 5);
 
     return 0;
 }
