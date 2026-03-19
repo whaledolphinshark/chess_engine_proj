77b54d4a8f2a98beae056e375cd31342421a53b6
whaledolphinshark <hongyilu123456@gmail.com>
2026-03-19 19:57:07 -0400

    added more tests for transposition table

--- chess_bot/test/test_transposition_table.c
+++ chess_bot/test/test_transposition_table.c
@@ -7,6 +7,18 @@
 
 #define NUM_ITEMS 100000
 
+uint64_t rand_uint64(){
+    int int_bits = sizeof(int) * 8;
+    int calls_needed = 64 / int_bits;
+
+    uint64_t result = 0;
+    for (int i = 0; i < calls_needed; i++){
+        result |= ((uint64_t) rand()) << (i * int_bits);
+    }
+
+    return result;
+}
+
 int main(int argc, char *argv[]){
     if (argc == 2){
         char *end_ptr;
@@ -34,17 +46,25 @@ int main(int argc, char *argv[]){
         printf("malloc() failed\n");
         return 1;
     }
+    for (int i = 0; i < NUM_ITEMS; i++){
+        keys[i] = rand_uint64();
+    }
     clock_t start = clock();
     for (int i = 0; i < NUM_ITEMS; i++){
-        keys[i] = (uint64_t)rand();
         tt_insert_item(table, keys[i], &i);
     }
     clock_t end = clock();
     printf("time to insert %d entries: %f seconds\n", NUM_ITEMS, ((double) (end - start)) / CLOCKS_PER_SEC);
 
+    // check number of entries
+    if (tt_get_num_items(table) != NUM_ITEMS){
+        printf("number of items in table does not equal number of items inserted: %d != %d", tt_get_num_items(table), NUM_ITEMS);
+    }
+
     // time to retrieve items
     int *values = malloc(sizeof(int) * NUM_ITEMS);
     if (values == NULL){
+        free(keys);
         tt_destroy_transposition_table(table);
         printf("malloc() failed\n");
         return 1;
@@ -59,13 +79,60 @@ int main(int argc, char *argv[]){
     // validate items retrieved
     for (int i = 0; i < NUM_ITEMS; i++){
         if (values[i] != i){
-            printf("error: %d != %d\n", values[i], i);
+            printf("item not retrieved: %d != %d\n", values[i], i);
             break;
+        }
+    }
+
+    // time to check keys
+    int *checks = malloc(sizeof(int) * NUM_ITEMS);
+    if (checks == NULL){
+        free(keys);
+        free(values);
+        tt_destroy_transposition_table(table);
+        return 1;
+    }
+    start = clock();
+    for (int i = 0; i < NUM_ITEMS; i++){
+        checks[i] = tt_is_key_in_table(table, keys[i]);
+    }
+    end = clock();
+    printf("time to check %d entries: %f seconds\n", NUM_ITEMS, ((double) (end - start)) / CLOCKS_PER_SEC);
+
+    // validate checks
+    for (int i = 0; i < NUM_ITEMS; i++){
+        if (checks[i] != 1){
+            printf("key not found in table: %lu\n", keys[i]);
+        }
+    }
+
+    // time remove operations
+    int *deletes = malloc(sizeof(int) * NUM_ITEMS);
+    if (deletes == NULL){
+        free(keys);
+        free(values);
+        free(checks);
+        tt_destroy_transposition_table(table);
+        return 1;
+    }
+    start = clock();
+    for (int i = 0; i < NUM_ITEMS; i++){
+        deletes[i] = tt_delete_item(table, keys[i]);
+    }
+    end = clock();
+    printf("time to delete %d entries: %f seconds\n", NUM_ITEMS, ((double) (end - start)) / CLOCKS_PER_SEC);
+
+    // validate deletes
+    for (int i = 0; i < NUM_ITEMS; i++){
+        if (deletes[i] != 1){
+            printf("entry not deleted: %lu\n", keys[i]);
         }
     }
 
     free(keys);
     free(values);
+    free(checks);
+    free(deletes);
     tt_destroy_transposition_table(table);
     return 0;
 }
