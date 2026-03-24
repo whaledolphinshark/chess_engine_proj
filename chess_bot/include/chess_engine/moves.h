1a12390dee7d2fb9052f5005d5d768502abb1130
whaledolphinshark <hongyilu123456@gmail.com>
2026-03-24 18:31:38 -0400

    forgot to add include file

--- chess_bot/include/chess_engine/moves.h
+++ chess_bot/include/chess_engine/moves.h
@@ -36,7 +36,8 @@ extern uint64_t left_border;
 extern uint64_t right_border;
 
 int mv_moves_equal(_move left, _move right);
-void mv_generate_moves(_board *board);
+void mv_generate_moves(_board *board, _move move_buffer[MAX_MOVES], int *move_count);
+void mv_generate_enemy_moves(_board *board, _move move_buffer[MAX_MOVES], int *move_count);
 _move mv_uci_to_move(char *move_uci, _board *board);
 void mv_move_to_uci(_move move, char *buffer);
 void mv_print_move(_move move, int new_line);
