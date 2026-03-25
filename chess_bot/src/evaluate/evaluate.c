5ff81b5569388594f27fe75c4828c31976bc111c
whaledolphinshark <hongyilu123456@gmail.com>
2026-03-25 14:35:45 -0400

    worked on evaluation function

--- chess_bot/src/evaluate/evaluate.c
+++ chess_bot/src/evaluate/evaluate.c
@@ -1,23 +1,14 @@
 #include "chess_engine/evaluate.h"
+#include "chess_engine/moves.h"
 #include "utils/bitboard_util.h"
 
 const int white_piece_values[12] = {10000, 100, 500, 300, 300, 900, -10000, -100, -500, -300, -300, -900};
 const int black_piece_values[12] = {-10000, -100, -500, -300, -300, -900, 10000, 100, 500, 300, 300, 900};
-const int white_mobility_values[12] = {0, 0, 15, 15, 15, 0, 0, 0, -15, -15, -15, 0};
-const int black_mobility_values[12] = {0, 0, -15, -15, -15, 0, 0, 0, 15, 15, 15, 0};
+const int mobility_values[12] = {0, 0, 15, 15, 15, 0, 0, 0, 15, 15, 15, 0};
 
 int ev_evaluate(_board *board){
     int score = 0;
-    const int *piece_values;
-    const int *mobility_values;
-    if (board->turn == WHITE){
-        piece_values = white_piece_values;
-        mobility_values = white_mobility_values;
-    }
-    else{
-        piece_values = black_piece_values;
-        mobility_values = black_mobility_values;
-    }
+    const int *piece_values = board->turn == WHITE ? white_piece_values : black_piece_values;
 
     uint64_t pieces = board->board;
     while (pieces != 0){
@@ -30,6 +21,16 @@ int ev_evaluate(_board *board){
         score += mobility_values[move.piece];
         if (move.capture != NONE){
             score++;
+        }
+    }
+    _move enemy_moves[MAX_MOVES];
+    int move_count = 0;
+    mv_generate_enemy_moves(board, enemy_moves, &move_count);
+    for (int i = 0; i < move_count; i++){
+        _move move = enemy_moves[i];
+        score -= mobility_values[move.piece];
+        if (move.capture != NONE){
+            score--;
         }
     }
 
