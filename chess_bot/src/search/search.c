1d0a2cb6906c550ccba3873b3ba9fc7060ef5c8d
whaledolphinshark <hongyilu123456@gmail.com>
2026-04-03 09:19:50 -0400

    tuned history heuristic

--- chess_bot/src/search/search.c
+++ chess_bot/src/search/search.c
@@ -1,5 +1,6 @@
 #include <limits.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "chess_engine/search.h"
 #include "chess_engine/board.h"
@@ -8,11 +9,25 @@
 #include "chess_engine/transposition_table.h"
 #include "utils/error_handling.h"
 
-static void order_moves(_board *restrict board, _move moves[restrict MAX_MOVES], const int move_count, _transposition_table *restrict transposition_table){
+#define MAX_HISTORY 65536
+
+static void update_history(const int depth, const int index, _move moves[restrict MAX_MOVES], _search_context *restrict context){
+    _move move = moves[index];
+    int bonus = depth * depth < MAX_HISTORY ? depth * depth : MAX_HISTORY;
+    context->history[move.piece][move.from][move.to] += bonus - (context->history[move.piece][move.from][move.to] * bonus) / MAX_HISTORY;
+    for (int j = 0; j < index; j++){
+        _move temp = moves[j];
+        if (temp.capture == NONE){
+            context->history[temp.piece][temp.from][temp.to] -= bonus - (context->history[temp.piece][temp.from][temp.to] * bonus) / MAX_HISTORY;
+        }
+    }
+}
+
+static void order_moves(_board *restrict board, _move moves[restrict MAX_MOVES], const int move_count, _search_context *restrict context){
     const uint64_t hash = board->zobrist_hash;
     _move best_move = {0, 0, NONE, NONE, NONE, NORMAL};
-    if (transposition_table != NULL && tt_contains_key(transposition_table, hash) == 1){
-        best_move = ((_tt_search_entry *)tt_get_item(transposition_table, hash))->best_move;
+    if (tt_contains_key(context->table, hash) == 1){
+        best_move = ((_tt_search_entry *)tt_get_item(context->table, hash))->best_move;
     }
     
     int values[move_count];
@@ -27,6 +42,9 @@ static void order_moves(_board *restrict board, _move moves[restrict MAX_MOVES],
         }
         if (move.special_move == PROMOTION){
             values[i] += 5000;
+        }
+        if (move.capture == NONE){
+            values[i] += context->history[move.piece][move.from][move.to];
         }
     }
 
@@ -46,7 +64,8 @@ static void order_moves(_board *restrict board, _move moves[restrict MAX_MOVES],
     }
 }
 
-static int quiescence_search(_board *board, int alpha, int beta){
+static int quiescence_search(_board *board, int alpha, int beta, _search_context *context, _search_stats *stats){
+    // stats->quiescent_nodes_visited++;
     int best_score = ev_evaluate(board);
     if (board->game_state != ONGOING || best_score >= beta){
         return best_score;
@@ -58,16 +77,16 @@ static int quiescence_search(_board *board, int alpha, int beta){
     _move moves[MAX_MOVES];
     int move_count;
     mv_generate_moves(board, moves, &move_count);
-    order_moves(board, moves, move_count, NULL);
+    order_moves(board, moves, move_count, context);
     int score = best_score;
     for (int i = 0; i < move_count; i++){
         _move move = moves[i];
-        if (move.capture == NONE && move.special_move != PROMOTION){
+        if (board->in_check == 0 && move.capture == NONE && move.special_move != PROMOTION){
             continue;
         }
 
         cb_make_move(board, move);
-        score = -quiescence_search(board, -beta, -alpha);
+        score = -quiescence_search(board, -beta, -alpha, context, stats);
         cb_undo_move(board);
 
         if (score > best_score){
@@ -84,91 +103,17 @@ static int quiescence_search(_board *board, int alpha, int beta){
     return best_score;
 }
 
-static int search(_board *board, int depth, int alpha, int beta, _transposition_table *transposition_table){
-    if (board->game_state != ONGOING){
-        return ev_evaluate(board);
-    }
-    if (depth == 0){
-        return quiescence_search(board, alpha, beta);
-    }
-
-    if (tt_contains_key(transposition_table, board->zobrist_hash) == 1){
-        _tt_search_entry *entry = (_tt_search_entry *)tt_get_item(transposition_table, board->zobrist_hash);
-        if (entry->depth >= depth && (entry->flag == EXACT || (entry->flag == LOWER_BOUND && entry->eval >= beta) || (entry->flag == UPPER_BOUND && entry->eval <= alpha))){
-            return entry->eval;
-        }
-    }
-    
-    _move moves[MAX_MOVES];
-    int move_count;
-    mv_generate_moves(board, moves, &move_count);
-    order_moves(board, moves, move_count, transposition_table);
-    int best_score = INT_MIN + 1;
-    _move best_move = moves[0];
-    _tt_flag flag = UPPER_BOUND;
-    for (int i = 0; i < move_count; i++){
-        _move move = moves[i];
-        cb_make_move(board, move);
-        int score;
-        if (i == 0 || beta - alpha == 1){
-            score = -search(board, depth - 1, -beta, -alpha, transposition_table);
-        }
-        else{
-            score = -search(board, depth - 1, -alpha - 1, -alpha, transposition_table);
-            if (score > alpha){
-                score = -search(board, depth - 1, -beta, -alpha, transposition_table);
-            }
-        }
-        cb_undo_move(board);
-
-        if (score > best_score){
-            best_score = score;
-            best_move = move;
-            if (score >= beta){
-                flag = LOWER_BOUND;
-                break;
-            }
-            if (score > alpha){
-                flag = EXACT;
-                alpha = score;
-            }
-        }
-    }
-
-    if (tt_contains_key(transposition_table, board->zobrist_hash) == 0 || ((_tt_search_entry *)tt_get_item(transposition_table, board->zobrist_hash))->depth < depth){
-        _tt_search_entry entry = {best_score, depth, best_move, flag};
-        tt_insert_item(transposition_table, board->zobrist_hash, &entry);
-    }
-
-    return best_score;
-}
-
-_move se_search(_board *board, int depth, int alpha, int beta, _transposition_table *transposition_table){
-    if (board == NULL || transposition_table == NULL){
-        eh_die("passed in null pointer");
-    }
-    if (tt_get_item_size(transposition_table) != sizeof(_tt_search_entry)){
-        eh_die("passed in invalid transposition table");
-    }
-
-    for (int i = 1; i <= depth; i++){
-        search(board, i, alpha, beta, transposition_table);
-    }
-
-    return ((_tt_search_entry *)tt_get_item(transposition_table, board->zobrist_hash))->best_move;
-}
-
-static int search_s(_board *board, int depth, int alpha, int beta, _transposition_table *transposition_table, _search_stats *stats){
+static int search(_board *board, int depth, int alpha, int beta, _search_context *context, _search_stats *stats){
     stats->nodes_visited++;
     if (board->game_state != ONGOING){
         return ev_evaluate(board);
     }
     if (depth == 0){
-        return quiescence_search(board, alpha, beta);
+        return quiescence_search(board, alpha, beta, context, stats);
     }
 
-    if (tt_contains_key(transposition_table, board->zobrist_hash) == 1){
-        _tt_search_entry *entry = (_tt_search_entry *)tt_get_item(transposition_table, board->zobrist_hash);
+    if (tt_contains_key(context->table, board->zobrist_hash) == 1){
+        _tt_search_entry *entry = (_tt_search_entry *)tt_get_item(context->table, board->zobrist_hash);
         if (entry->depth >= depth && (entry->flag == EXACT || (entry->flag == LOWER_BOUND && entry->eval >= beta) || (entry->flag == UPPER_BOUND && entry->eval <= alpha))){
             return entry->eval;
         }
@@ -177,7 +122,7 @@ static int search_s(_board *board, int depth, int alpha, int beta, _transpositio
     _move moves[MAX_MOVES];
     int move_count;
     mv_generate_moves(board, moves, &move_count);
-    order_moves(board, moves, move_count, transposition_table);
+    order_moves(board, moves, move_count, context);
     int best_score = INT_MIN + 1;
     _move best_move = moves[0];
     _tt_flag flag = UPPER_BOUND;
@@ -186,13 +131,13 @@ static int search_s(_board *board, int depth, int alpha, int beta, _transpositio
         cb_make_move(board, move);
         int score;
         if (i == 0 || beta - alpha == 1){
-            score = -search_s(board, depth - 1, -beta, -alpha, transposition_table, stats);
+            score = -search(board, depth - 1, -beta, -alpha, context, stats);
         }
         else{
-            score = -search_s(board, depth - 1, -alpha - 1, -alpha, transposition_table, stats);
+            score = -search(board, depth - 1, -alpha - 1, -alpha, context, stats);
             if (score > alpha){
                 stats->researches++;
-                score = -search_s(board, depth - 1, -beta, -alpha, transposition_table, stats);
+                score = -search(board, depth - 1, -beta, -alpha, context, stats);
             }
         }
         cb_undo_move(board);
@@ -202,6 +147,7 @@ static int search_s(_board *board, int depth, int alpha, int beta, _transpositio
             best_move = move;
             if (score >= beta){
                 flag = LOWER_BOUND;
+                update_history(depth, i, moves, context);
                 break;
             }
             if (score > alpha){
@@ -211,26 +157,45 @@ static int search_s(_board *board, int depth, int alpha, int beta, _transpositio
         }
     }
 
-    if (tt_contains_key(transposition_table, board->zobrist_hash) == 0 || ((_tt_search_entry *)tt_get_item(transposition_table, board->zobrist_hash))->depth < depth){
+    if (tt_contains_key(context->table, board->zobrist_hash) == 0 || ((_tt_search_entry *)tt_get_item(context->table, board->zobrist_hash))->depth < depth){
         _tt_search_entry entry = {best_score, depth, best_move, flag};
-        tt_insert_item(transposition_table, board->zobrist_hash, &entry);
+        tt_insert_item(context->table, board->zobrist_hash, &entry);
     }
 
     return best_score;
 }
 
-_move se_search_stats(_board *board, int depth, int alpha, int beta, _transposition_table *transposition_table, _search_stats *stats){
-    if (board == NULL || transposition_table == NULL){
+_move se_search(_board *board, int depth, int alpha, int beta, _search_context *context, _search_stats *stats){
+    if (board == NULL || stats == NULL){
         eh_die("passed in null pointer");
     }
-    if (tt_get_item_size(transposition_table) != sizeof(_tt_search_entry)){
-        eh_die("passed in invalid transposition table");
+    if (context->table == NULL){
+        eh_die("context not initialized");
+    }
+    if (tt_get_item_size(context->table) != sizeof(_tt_search_entry)){
+        eh_die("context initialized incorrectly");
     }
 
     for (int i = 1; i <= depth; i++){
-        search_s(board, i, alpha, beta, transposition_table, stats);
+        search(board, i, alpha, beta, context, stats);
     }
 
-    stats->eval = ((_tt_search_entry *)tt_get_item(transposition_table, board->zobrist_hash))->eval;
-    return ((_tt_search_entry *)tt_get_item(transposition_table, board->zobrist_hash))->best_move;
+    _tt_search_entry *entry = (_tt_search_entry *)tt_get_item(context->table, board->zobrist_hash);
+    stats->eval = entry->eval;
+    return entry->best_move;
+}
+
+void se_init_search_context(_search_context *context){
+    context->table = tt_create_transposition_table(sizeof(_tt_search_entry));
+    for (int i = 0; i < 12; i++){
+        for (int j = 0; j < 64; j++){
+            for (int k = 0; k < 64; k++){
+                context->history[i][j][k] = 0;
+            }
+        }
+    }
+}
+
+void se_destroy_search_context(_search_context *context){
+    tt_destroy_transposition_table(context->table);
 }
