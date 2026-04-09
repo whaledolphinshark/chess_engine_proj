7804ea30ecf18799695096a2509124769ada6253
whaledolphinshark <hongyilu123456@gmail.com>
2026-04-09 18:31:29 -0400

    just testing how to remove file

--- chess_bot/test/alt_bot/alt_bot.h
+++
@@ -1,9 +0,0 @@
-#ifndef     ALT_H
-#define     ALT_H
-
-#include "chess_engine/chess_types.h"
-#include "chess_engine/search.h"
-
-_move alt_search(_board *board, int depth, _search_context *context, _search_stats *stats);
-
-#endif
