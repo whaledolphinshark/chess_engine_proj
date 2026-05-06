dd120ed7189ab38978cae451b8973e7e5e7342d7
whaledolphinshark <hongyilu123456@gmail.com>
2026-05-06 16:26:42 -0400

    worked a little on the code for connecting to lichess

--- chess_bot/src/main.c
+++ chess_bot/src/main.c
@@ -6,25 +6,65 @@
 typedef struct{
     char *data;
     size_t size;
+    int length;
 }_response;
 
 size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp) {
-    size_t total_size = size * nmemb;
+    const size_t total_length = size * nmemb;
     _response *response = (_response *)userp;
 
-    size_t new_size = response->size == 0 ? total_size + 1 : response->size + total_size;
-    response->data = realloc(response->data, new_size);
-    if (response->data == NULL){
-        fprintf(stderr, "realloc() failed");
-        return 0;
+    const size_t new_length = response->length == 0 ? total_length + 1 : response->length + total_length;
+    if (new_length > response->size){
+        response->size = new_length;
+        response->data = realloc(response->data, new_length);
+        if (response->data == NULL){
+            fprintf(stderr, "realloc() failed");
+            return 0;
+        }
     }
 
-    memcpy(&(response->data[response->size]), contents, total_size);
-    response->size += total_size;
-    response->data[response->size] = '\0';
-    printf("debug: %s\n", response->data);
+    memcpy(&(response->data[response->length]), contents, total_length);
+    int i = response->length;
+    response->length = new_length;
+    response->data[response->length] = '\0';
+    char *start = response->data;
 
-    return total_size;
+    // check for responses
+    while (i < response->length){
+        if (response->data[i] == '\n'){
+            // find type
+            char *ptr_1 = strstr(start, "\"type\"");
+            char *ptr_2 = strstr(start, "\"gameStart\"");
+            const int type_index = ptr_1 - response->data;
+            const int game_start_index = ptr_2 - response->data;
+            if (ptr_1 != NULL && ptr_2 != NULL && type_index < i && game_start_index - type_index == 8){
+                // new game has started, check if my turn first
+                char *ptr_3 = strstr(start, "\"isMyTurn\"");
+                const int is_my_turn_index = ptr_3 - response->data;
+                if (ptr_3 != NULL && is_my_turn_index < i){
+                    // read value and if true, make move else dont
+                    if (response->data[is_my_turn_index + 12] == 't'){
+                        // make move
+
+                    }
+                }
+
+                return CURL_WRITEFUNC_ERROR;
+            }
+
+            start = response->data + i + 1;
+        }
+        i++;
+    }
+
+    // resize data
+    if (start != response->data){
+        const int remaining_length = response->length - (start - response->data);
+        memmove(response->data, start, sizeof(char) * remaining_length);
+        response->length = remaining_length;
+    }
+
+    return total_length;
 }
 
 int main(){
@@ -36,6 +76,7 @@ int main(){
         return 1;
     }
     response.size = 0;
+    response.length = 0;
     curl_global_init(CURL_GLOBAL_ALL);
     CURL *curl = curl_easy_init();
 
